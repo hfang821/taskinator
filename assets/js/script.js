fd0884a1445642dc61bfded1fd01b18c863a03b5
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-03-11 12:42:31 -0500

    added the feature of moving tasks to different status"

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector ("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event){
 
@@ -161,7 +163,28 @@ var editTask = function (taskId){
    
 };
 
+var taskStatusChangeHandler = function(event){
+    //get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    //get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    //find the parent item element based on the id
+    var taskSelected = document.querySelector (".task-item[data-task-id='" + taskId + "']");
+    
+    if (statusValue === "to do"){
+        //appendChild method will move the task item from original location in the DOM into other <ul>
+        tasksToDoEl.appendChild(taskSelected);
+    } else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    } else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild (taskSelected);
+    }
+};
+
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change",taskStatusChangeHandler);
 
 
 
